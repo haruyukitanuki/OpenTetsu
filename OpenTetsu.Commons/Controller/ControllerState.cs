37f35ca6470ad2af79b2d98075c558c7230479be
Haruyuki Tanukiji <haruyuki@tanu.ch>
2024-06-21 14:49:02 +0800

    ノッチの値を分ける

--- OpenTetsu.Commons/Controller/ControllerState.cs
+++ OpenTetsu.Commons/Controller/ControllerState.cs
@@ -7,6 +7,12 @@ public class ControllerState
     [JsonProperty("notch")]
     public int Notch;
 
+    [JsonProperty("bNotch")] 
+    public int BNotch;
+    
+    [JsonProperty("pNotch")] 
+    public int PNotch;
+
     [JsonProperty("reverser")]
     public int Reverser;
 }
